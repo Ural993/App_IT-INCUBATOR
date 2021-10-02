69b517fa971b845d5df52298ba91e4dd93e60137
unknown <you@example.com>
2021-10-02 10:39:36 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className='header'>
+        Header
       </header>
+      <nav className='nav'>
+        <div className="container">
+          <div><a href="#">Profile</a></div>
+          <div><a href="#">Messages</a></div>
+          <div><a href="#">News</a></div>
+          <div><a href="#">Musics</a></div>
+          <div><a href="#">Settings</a></div>
+        </div>
+      </nav>
+      <div className="content">
+        <div className='avatar'>
+          <img src="https://vraki.net/sites/default/files/inline/images/1_42.jpg" alt="" />
+        </div>
+      </div>
+      <footer className='footer'>
+        Footer
+      </footer>
+
     </div>
   );
 }
